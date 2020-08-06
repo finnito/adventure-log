b107f6351bdbb10b89d3b24344a7a6a55bd2f444
Finn LeSueur <finn@lesueur.nz>
2020-08-06 12:03:53 +1200

    Adds search controller & view

--- addons/adventure_log/finnito/places-module/src/Http/Controller/SearchController.php
+++ addons/adventure_log/finnito/places-module/src/Http/Controller/SearchController.php
@@ -15,7 +15,15 @@ class SearchController extends PublicController
      */
     public function search(PlaceModel $model, Request $request)
     {
-        return $model->search($request->input("q"))->get();
+        $results = $model->search($request->q)->get();
+        $this->template->set("meta_title", "Search results for '" . $request->q . "'");
+        return $this->view->make(
+            'finnito.module.places::places/search',
+            [
+                'results' => $results,
+                "query" => $request->q
+            ]
+        );
     }
 
 }

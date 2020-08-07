b5a19ebd4ac6e084870347948688dbdfe00e4e46
Finn LeSueur <finn@lesueur.nz>
2020-08-07 14:36:08 +1200

    Renames file & fixes pylint errors

--- load_huts_geojson.py
+++ load_huts_geojson.py
@@ -1,5 +1,14 @@
 #!/Users/finnlesueur/.pyenv/shims/python3
 
+""" Author: Finn LeSueur
+
+This script parses the geojson file
+containing all huts in New Zealand.
+
+It can be exported here:
+https://catalogue.data.govt.nz/dataset/doc-huts
+"""
+
 import pymysql.cursors
 from dotenv import load_dotenv
 from pathlib import Path
@@ -9,13 +18,15 @@ import os
 
 
 def main():
+    """ The brains
+    """
     # Load the .env file
     env_path = Path('.') / '.env'
     load_dotenv(dotenv_path=env_path)
 
     # Load the DOC Huts geojson
-    with open("DOC_Huts.geojson") as f:
-        gj = geojson.load(f)
+    with open("DOC_Huts.geojson") as file:
+        huts = geojson.load(file)
 
     # Connect to MySQL
     connection = pymysql.connect(host=os.getenv("DB_HOST"),
@@ -25,7 +36,7 @@ def main():
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
 
-    for feature in gj["features"]:
+    for feature in huts["features"]:
         with connection.cursor() as cursor:
             # Create a new record
             sql = """
@@ -64,32 +75,34 @@ def main():
                     %s
                 )"""
             cursor.execute(sql, (
-                    time.strftime('%Y-%m-%d %H:%M:%S'),
-                    1,
-                    feature["properties"]["name"],
-                    slugify(feature["properties"]["name"]),
-                    slugify(feature["properties"]["place"]),
-                    int(feature["properties"]["FID"]),
-                    feature["properties"]["place"],
-                    feature["properties"]["region"],
-                    feature["properties"]["facilities"],
-                    feature["properties"]["staticLink"],
-                    int(feature["properties"]["assetId"]),
-                    feature["properties"]["dateLoadedToGIS"],
-                    float(feature["geometry"]["coordinates"][1]),
-                    float(feature["geometry"]["coordinates"][0]),
-                    feature["properties"]["introductionThumbnail"]
-                )
-            )
+                time.strftime('%Y-%m-%d %H:%M:%S'),
+                1,
+                feature["properties"]["name"],
+                slugify(feature["properties"]["name"]),
+                slugify(feature["properties"]["place"]),
+                int(feature["properties"]["FID"]),
+                feature["properties"]["place"],
+                feature["properties"]["region"],
+                feature["properties"]["facilities"],
+                feature["properties"]["staticLink"],
+                int(feature["properties"]["assetId"]),
+                feature["properties"]["dateLoadedToGIS"],
+                float(feature["geometry"]["coordinates"][1]),
+                float(feature["geometry"]["coordinates"][0]),
+                feature["properties"]["introductionThumbnail"]
+            ))
             connection.commit()
 
 def slugify(string):
+    """ Turns a string into a quick
+    and dirty URL safe slug.
+    """
     if string is None:
         string = ""
     non_url_safe = ['"', '#', '$', '%', '&', '+',
-                        ',', '/', ':', ';', '=', '?',
-                        '@', '[', '\\', ']', '^', '`',
-                        '{', '|', '}', '~', "'"]
+                    ',', '/', ':', ';', '=', '?',
+                    '@', '[', '\\', ']', '^', '`',
+                    '{', '|', '}', '~', "'"]
     translate_table = {ord(char): u'' for char in non_url_safe}
     slug = string.translate(translate_table)
     slug = slug.split()

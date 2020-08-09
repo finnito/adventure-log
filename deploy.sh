06a5d82702fae82e44dd609726fc63db15c65789
Finn LeSueur <finn@lesueur.nz>
2020-08-09 23:20:55 +1200

    Make a deploy script
    Not sure if it will resolve the issues? WIP #12

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd /srv/adventurelog.nz/
+sudo -u finn git checkout master
+sudo -u finn git stash
+sudo -u finn git pull origin master
+sudo -u www-data php artisan migrate --addon=finnito.module.places
+sudo -u www-data php artisan cache:clear
+sudo -u www-data php artisan streams:cleanup
+sudo -u www-data php artisan streams:compile
+sudo -u www-data php artisan streams:index
+sudo -u www-data php artisan httpcache:clear
+sudo -u www-data php artisan httpcache:warm

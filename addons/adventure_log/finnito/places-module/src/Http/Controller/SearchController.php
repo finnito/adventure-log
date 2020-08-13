17ec25a1320cf4b868c28e343adde57e8d843850
Finn LeSueur <finn@lesueur.nz>
2020-08-13 19:55:49 +1200

    Disables pagination on search results
    Closes #24

--- addons/adventure_log/finnito/places-module/src/Http/Controller/SearchController.php
+++ addons/adventure_log/finnito/places-module/src/Http/Controller/SearchController.php
@@ -15,15 +15,24 @@ class SearchController extends PublicController
      */
     public function search(PlaceModel $model, Request $request)
     {
-        $hutResults = $model->search($request->q)->get();
-        $placeResults = $model->newQuery()->where("place", "like", "%".$request->q."%")->groupBy("place")->get();
+        
+        $hutResults = $model
+            ->newQuery()
+            ->where("name", "LIKE", "%".$request->q."%")
+            ->get();
+        $placeResults = $model
+            ->newQuery()
+            ->where("place", "like", "%".$request->q."%")
+            ->groupBy("place")
+            ->get();
         $this->template->set("meta_title", "Search results for '" . $request->q . "'");
         return $this->view->make(
             'finnito.module.places::places/search',
             [
                 'results' => $hutResults,
                 'placeResults' => $placeResults,
-                "query" => $request->q
+                "query" => $request->q,
+                "resultCount" => $hutResults->count() + $placeResults->count()
             ]
         );
     }

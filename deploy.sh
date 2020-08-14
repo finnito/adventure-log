9bdeb2a3f295ec72531d3e8dc603c0d984f6a61e
Finn LeSueur <finn@lesueur.nz>
2020-08-15 00:44:57 +1200

    Adds composer:install
    Should close #12

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cd /srv/adventurelog.nz/
 sudo -u finn git checkout master
 sudo -u finn git stash
 sudo -u finn git pull origin master
+sudo -u finn composer install
 sudo -u www-data php artisan migrate --addon=finnito.module.places
 sudo -u www-data php artisan cache:clear
 sudo -u www-data php artisan streams:cleanup

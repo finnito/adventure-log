7e26aa3485799738a68ba8d5da5fed1cd2cb0c50
Finn LeSueur <finn@lesueur.nz>
2020-08-25 19:05:33 +1200

    [Chore]: Rework to use CURL

--- addons/adventure_log/finnito/places-module/src/Feedback/Form/FeedbackFormHandler.php
+++ addons/adventure_log/finnito/places-module/src/Feedback/Form/FeedbackFormHandler.php
@@ -33,43 +33,48 @@ class FeedbackFormHandler
             "blog" => env("AKISMET_BLOGURL")
 
         ]);
-        if ($akismet->validateKey()) {
-            if ($akismet->isSpam()) {
-                $bag->info("Your feedback was marked as SPAM. If this is an error, please email Finn.");
-            } else {
-                $bag->success("Thank you for your feedback! You can view it <a href='https://github.com/finnito/adventure-log/issues'>by clicking here</a>.");
-                if (!$builder->canSave()) {
-                    return;
-                }
-
-                $access_token = env("GITHUB_ACCESS_TOKEN");
-                $url = "https://api.github.com/repos/finnito/adventure-log/issues";
-                $title = htmlspecialchars(stripslashes($values["title"]), ENT_QUOTES);
-                $body = htmlspecialchars(stripslashes($values["description"]), ENT_QUOTES);
-                $post = array(
-                    "title" => $title,
-                    "body" => $body,
-                    "assignees" => array("finnito"),
-                    "labels" => array("Website Feedback")
-                );
-                $opts = [
-                    'http' => [
-                            'method' => 'POST',
-                            'header' => [
-                                    'User-Agent: PHP',
-                                    'Content-type: application/x-www-form-urlencoded',
-                                    'Authorization: token ' . $access_token
-                            ],
-                            'content' => json_encode($post)
-                    ]
-                ];
-                $context = stream_context_create($opts);
-                $content = json_decode(file_get_contents($url, false, $context));
-                $builder->saveForm();
-                $cache->purge('/feedback');
-            }
-        } else {
+        if (!$akismet->validateKey()) {
             $bag->info("There was an error with Akismet");
-        }    
+            return;
+        }
+
+        if ($akismet->isSpam()) {
+            $bag->info("Your feedback was marked as SPAM. If this is an error, please email Finn.");
+            return;
+        }
+
+        if (!$builder->canSave()) {
+            $bag->info("There was an issue saving your feedback. Please email Finn!");
+            return;
+        }
+        $builder->saveForm();
+
+        $cache->purge('/feedback');
+        
+        $url = "https://api.github.com/repos/finnito/adventure-log/issues";
+        $curl = curl_init($url);
+        $payload = json_encode(array(
+            "title" => $values["title"],
+            "body" => $values["description"],
+            "assignees" => array("finnito"),
+            "labels" => array("Website Feedback")
+        ));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'User-Agent: PHP',
+            'Content-Type:application/json',
+            'Authorization: token ' . env("GITHUB_ACCESS_TOKEN"),
+            'Accept: application/vnd.github.v3+json'
+        ));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $result = json_decode(curl_exec($curl), true);
+
+        if (array_key_exists("message", $result)) {
+            $bag->info("Your log could not be logged to GitHub, but it has been saved. Thank you!\n".$result["message"]);
+            return;
+        }
+        
+        $bag->success("Thank you for your feedback! You can view it <a href='".$result["html_url"]."'>by clicking here</a>.");
+        curl_close($curl);
     }
 }
